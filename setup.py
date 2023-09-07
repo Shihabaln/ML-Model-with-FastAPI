51a2df880b954f9eec9b24173c5d4e88cc230dc3
Shihab Al Nassri <shihab@Esbaar.com>
2023-09-07 23:51:01 +0400

    updated requirments

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-import setuptools
+from pathlib import Path
+import setuptools 
+
+# Load packages from requirements.txt
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt"), "r") as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
 
 setuptools.setup(
     name="starter",
